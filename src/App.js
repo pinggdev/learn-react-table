2f0009da0787bde9da1d286d79813f14f273b630
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-10-21 01:37:49 +0700

    table sorting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BasicTable } from "./components/BasicTable";
+import { SortingTable } from "./components/SortingTable";
 
 function App() {
   return (
     <div className="App">
-      <BasicTable />
+      {/* <BasicTable /> */}
+      <SortingTable />
     </div>
   );
 }
